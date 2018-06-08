bb86b3ef54d1812d43af713c733b75804aef5075
HassakuTb <hassaku.sc@gmail.com>
2018-06-08 15:36:38 +0900

    implement dash logic

---
+++ Assets/Scripts/Action/Dash.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace ConcleteAction {
+
+    /// <summary>
+    /// 地上ダッシュ
+    /// </summary>
+    [CreateAssetMenu(fileName = "Dash", menuName = "ScriptableObject/Action/Dash")]
+    public class Dash : Action {
+
+        public float Velocity;
+
+        public override bool Trigger() {
+            return this.Agent.IsGround && this.FixedInputController.InputButtonTable["Dash"].PressedFrame > 0;
+        }
+
+        public override void OnTrigger() {
+            Vector2 velocity = this.Agent.RigidbodyCache.velocity;
+            if(this.Agent.Direction == Agent.AgentDirection.Left) {
+                if (velocity.x > -Velocity) {
+                    this.Agent.RigidbodyCache.velocity = new Vector2(-Velocity, velocity.y);
+                }
+            }
+            else {
+                if (velocity.x < Velocity) {
+                    this.Agent.RigidbodyCache.velocity = new Vector2(Velocity, velocity.y);
+                }
+            }
+        }
+    }
+
+}
