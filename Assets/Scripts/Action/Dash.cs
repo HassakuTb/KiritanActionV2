96dea1840bf4ced3cf3a7b7087ecb0d1fac4b0e6
HassakuTb <hassaku.sc@gmail.com>
2018-06-08 20:05:20 +0900

    turn at dashing

--- Assets/Scripts/Action/Dash.cs
+++ Assets/Scripts/Action/Dash.cs
@@ -11,13 +11,34 @@ namespace ConcleteAction {
 
         public float Velocity;
 
+        private bool isPressedHorizontal;
+        Agent.AgentDirection direction;
+
         protected override bool Trigger() {
-            return this.Agent.IsGround && Input.GetButtonDown("Dash");
+            if(this.Agent.IsGround && Input.GetButtonDown("Dash")) {
+                if(Input.GetAxis("Horizontal") > 0.1) {
+                    isPressedHorizontal = true;
+                    direction = Agent.AgentDirection.Right;
+                }
+                else if(Input.GetAxis("Horizontal") < -0.1) {
+                    isPressedHorizontal = true;
+                    direction = Agent.AgentDirection.Left;
+                }
+                else {
+                    isPressedHorizontal = false;
+                }
+                return true;
+            }
+            return false;
         }
 
         protected override void OnTrigger() {
             Vector2 velocity = this.Agent.RigidbodyCache.velocity;
-            if(this.Agent.Direction == Agent.AgentDirection.Left) {
+            Agent.AgentDirection actualDirection = this.Agent.Direction;
+            if (isPressedHorizontal) actualDirection = direction;
+
+
+            if (actualDirection == Agent.AgentDirection.Left) {
                 if (velocity.x > -Velocity) {
                     this.Agent.RigidbodyCache.velocity = new Vector2(-Velocity, velocity.y);
                 }
@@ -27,6 +48,7 @@ namespace ConcleteAction {
                     this.Agent.RigidbodyCache.velocity = new Vector2(Velocity, velocity.y);
                 }
             }
+            this.Agent.SetDirection(actualDirection);
 
             this.Agent.DashStatus.OnDash();
         }
