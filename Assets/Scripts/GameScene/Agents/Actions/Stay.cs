eeb6590ee16680471b5bf0b6a2cab5b0d107026d
HassakuTb <hassaku.sc@gmail.com>
2018-06-15 22:31:27 +0900

    create Stay action and delete Friction action

---
+++ Assets/Scripts/GameScene/Agents/Actions/Stay.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+namespace GameScene.Agents.Actions {
+    /// <summary>
+    /// 地上で無入力
+    /// </summary>
+    public class Stay : Action {
+
+        public float Friction;
+
+        protected override bool Trigger() {
+            return this.Agent.IsGround && Mathf.Abs(Input.GetAxis("Horizontal")) < 0.1;
+        }
+
+        protected override void OnTrigger() {
+            Vector2 velocity = this.Agent.RigidbodyCache.velocity;
+            if (velocity.x > 0) {
+                this.Agent.RigidbodyCache.velocity = new Vector2(velocity.x - Friction > 0 ? velocity.x - Friction : 0, velocity.y);
+            }
+            else if (velocity.x < 0) {
+                this.Agent.RigidbodyCache.velocity = new Vector2(velocity.x + Friction < 0 ? velocity.x + Friction : 0, velocity.y);
+            }
+
+        }
+    }
+}
