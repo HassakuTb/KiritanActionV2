caa7aac84440dcd302411c6efb83492596b75997
HassakuTb <hassaku.sc@gmail.com>
2018-06-15 22:36:03 +0900

    air move no effect when dashing

--- Assets/Scripts/GameScene/Agents/Actions/AirMove.cs
+++ Assets/Scripts/GameScene/Agents/Actions/AirMove.cs
@@ -14,6 +14,7 @@ namespace GameScene.Agents.Actions {
 
         protected override bool Trigger() {
             if (this.Agent.IsGround) return false;
+            if (this.Agent.DashStatus.IsDashing) return false;
             if (Input.GetAxis("Horizontal") < -0.1) {
                 this.direction = Agent.AgentDirection.Left;
                 return true;
